4ee8300c6b81f6de9f990fecb2be6c1eb1fb54fd
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-08 17:16:32 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    const stringArray = stringToReverse.split('');
+    const reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
