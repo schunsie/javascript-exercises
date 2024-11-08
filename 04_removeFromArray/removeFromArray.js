e57daef05b4e77a3f46e7a18c7bd25bd3ebdad4c
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-08 19:27:22 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(rawArray) {
+    const itemsToRemove = Array.prototype.slice.call(arguments, 1);
+    
+    const array = rawArray.filter(isWanted);
+    return array;
+
+
+    function isWanted(item) {
+        for (let unwantedItem of itemsToRemove) {
+            if (item === unwantedItem)
+                return false;
+        }
+        return true;
+    }
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
