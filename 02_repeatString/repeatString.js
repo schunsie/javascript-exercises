83404d0819908d5cb7bc3f948ede61b07c9c814b
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-08 16:54:35 +0100

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, amountOfRepeats) {
+    if (amountOfRepeats < 0) return 'ERROR';
+    
+    let repeatedString = '';
+    for (let i = 0; i < amountOfRepeats; i++) {
+        repeatedString += stringToRepeat
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
