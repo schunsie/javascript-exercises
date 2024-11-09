987d7f58588c826fb650448f01439908ff2b6545
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-09 17:13:08 +0100

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
+    if (!numbers.every(validateArgument)) {
+        return 'ERROR';
+    }
 
+    sortRange(numbers);
+    
+    let startNum = numbers[0];
+    let endNum = numbers[1];
+    let sum = 0;
+    
+    for (let num = startNum; num <= endNum; num++) {
+        sum += num;
+    }
+    return sum;
+}
+
+function validateArgument(arg) {
+    if (typeof(arg) == 'number' && arg >= 0) {
+        return Number.isInteger(arg) ? true : false;
+    }
+
+    return false;
+};
+
+function sortRange(array) {
+    if (array[0] > array[1]) {
+        array.reverse();
+    }
+    return array;
 };
 
 // Do not edit below this line
