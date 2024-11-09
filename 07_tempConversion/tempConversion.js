093bbf5bfd1f734346c9f4fd2ce753700e641b94
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-09 20:02:05 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit - 32) * 5/9;
+  return roundToOneDecimal(tempCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = tempCelsius * 9/5 + 32; 
+  return roundToOneDecimal(tempFahrenheit); 
 };
+
+function roundToOneDecimal(float) {
+  return Math.round(float * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
