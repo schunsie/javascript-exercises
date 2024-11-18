44e141b4f0f5b902bd2b9f8a7bf42949baec3f09
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-18 11:53:59 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (rawWord) {
+    const word = rawWord
+    .split('')
+    .filter(char => !".,?!".includes(char))
+    .filter(char => char != ' ')
+    .join('')
+    .toLowerCase();
 
+    const reversed = word.split('').reverse().join('');
+    return word == reversed;
 };
 
 // Do not edit below this line
