60ccaba445c0768183cd0d37ff2441ba296fdee8
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-18 16:30:05 +0100

    Completed java-script exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = (new Date()).getFullYear();
+    array.sort((a, b) => {
+        return calcAge(b) - calcAge(a);
+    });
 
+    function calcAge(person) {
+        return person.yearOfDeath ? 
+        age = person.yearOfDeath - person.yearOfBirth
+        : age = currentYear - person.yearOfBirth;
+    }
+
+    return array[0];
 };
 
 // Do not edit below this line
